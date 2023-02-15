772c15b24ec29adeff094eb9becdbbede7deda5d
stanw0ng <user.stanley@gmail.com>
2023-02-15 13:18:04 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,20 @@
+// Weather
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+// Temperature
+
 const temperature = -12;
 
 if (temperature < 0) {
@@ -9,3 +26,12 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+// Logical Citizen
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
